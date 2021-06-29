765001515e9ee59e99d02ff2b2a2c3ca9f9d7a81
Patrick Kennedy <pkennedytx1@gmail.com>
2021-06-28 23:51:27 -0500

    moved around files in order to meet structure requirents for firebase

--- public/app.js
+++ public/app.js
@@ -20,7 +20,6 @@ if( document.readyState !== 'loading' ) {
 }
 
 const updateUserInformation = (userData) => {
-    console.log(userData);
     const userName = document.getElementById('user-name');
     const userDetails = document.getElementById('user-text');
     const userImage = document.getElementById('user-image');
@@ -31,7 +30,6 @@ const updateUserInformation = (userData) => {
 }
 
 const validateEmail = (email) => {
-    console.log('hello')
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
@@ -40,7 +38,6 @@ const handleFormValidation = (data) => {
     document.getElementById('form-error').innerText = '';
     if (data.name === '' || data.email === '' || data.message == '') {
         document.getElementById('form-error').innerText = 'All fields must be filled out.';
-        console.log('hello')
         return false
     }
     if (!validateEmail(data.email)) {
@@ -50,10 +47,29 @@ const handleFormValidation = (data) => {
     return true;
 }
 
+const writeContactUser = (data) => {
+    try {
+        firebase.database().ref('contactUsers').push({
+            username: data.name,
+            email: data.email,
+            message: data.message,
+        });
+        document.getElementById('form-success').innerText = 'Message Successfully Saved!';
+        const form = document.querySelector('form');
+        form.reset();
+        setTimeout(() => {
+            document.getElementById('form-success').innerText = '';
+        }, 3000);
+    } catch (error) {
+        document.getElementById('form-error').innerText = 'There was an error saving your message.';
+        console.error(error);
+    }
+}
+
 const handleFormSubmit = (e) => {
     const form = document.querySelector('form');
     const data = Object.fromEntries(new FormData(form).entries());
     if (handleFormValidation(data)) {
-        
+        writeContactUser(data);
     };
 }
