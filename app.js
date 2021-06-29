b4313eb42fd97372da56f524eeafc7fb0218c734
Patrick Kennedy <pkennedytx1@gmail.com>
2021-06-28 23:12:44 -0500

    form validation

--- app.js
+++ app.js
@@ -29,3 +29,31 @@ const updateUserInformation = (userData) => {
     userDetails.innerText = `Hello, my name is ${userData.name.first} ${userData.name.last}. I live in ${userData.location.city}, ${userData.location.state} in ${userData.location.country}.`
     userImage.src = `${userData.picture.large}`;
 }
+
+const validateEmail = (email) => {
+    console.log('hello')
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const handleFormValidation = (data) => {
+    document.getElementById('form-error').innerText = '';
+    if (data.name === '' || data.email === '' || data.message == '') {
+        document.getElementById('form-error').innerText = 'All fields must be filled out.';
+        console.log('hello')
+        return false
+    }
+    if (!validateEmail(data.email)) {
+        document.getElementById('form-error').innerText = 'Invalid Email';
+        return false;
+    }
+    return true;
+}
+
+const handleFormSubmit = (e) => {
+    const form = document.querySelector('form');
+    const data = Object.fromEntries(new FormData(form).entries());
+    if (handleFormValidation(data)) {
+        
+    };
+}
