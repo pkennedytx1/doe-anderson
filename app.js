091c70ff1ba3594adad6e31f70c5ea4a31d433d9
Patrick Kennedy <pkennedytx1@gmail.com>
2021-06-28 22:03:04 -0500

    added the image and the user information to render on load as well as clicking on the media icons

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const getNewUserData = async () => {
-    const response = await axios.get('https://randomuser.me/api');
-    console.log(response);
+    try {
+        const response = await axios.get('https://randomuser.me/api');
+        updateUserInformation(response.data.results[0]);
+    } catch (error) {
+        throw new Error('An error occured retriecing user information.');
+    }
 }
 
 const handleGenerateNewUser = () => {
     getNewUserData();
+}
+
+if( document.readyState !== 'loading' ) {
+    handleGenerateNewUser();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        handleGenerateNewUser();
+    });
+}
+
+const updateUserInformation = (userData) => {
+    console.log(userData);
+    const userName = document.getElementById('user-name');
+    const userDetails = document.getElementById('user-text');
+    const userImage = document.getElementById('user-image');
+
+    userName.innerText = `${userData.name.title} ${userData.name.first} ${userData.name.last}`;
+    userDetails.innerText = `Hello, my name is ${userData.name.first} ${userData.name.last}. I live in ${userData.location.city}, ${userData.location.state} in ${userData.location.country}.`
+    userImage.src = `${userData.picture.large}`;
 }
